f245081840ce4412d0297b8b728fe70885858e38
AlexandruMunteanu89 <sasamunteanu18@gmail.com>
2026-05-03 13:36:56 +0200

    Creato ul con la lista iniziale, piu il bottone remove

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,7 +1,28 @@
+import { use, useState } from 'react'
+
 function Articles() {
+    const listaIniziale = [
+        "Citroën",
+        "Peugeout",
+        "Opel"
+    ]
+    const [tasks, setTasks] = useState(listaIniziale)
+    const [newTask, setNewTask] = useState("")
+    //console.log(newTask);
+
   return (
     <>
-        <h1>Article title</h1>
+        <div>
+            <h1>La lista di articoli</h1>
+            <div>
+                <ul className="list-group">
+                    {tasks.map((task, i) => (
+                    <li className="list-group-item" key={`${i}-task`}>{task} <button className='btn btn-danger'><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button></li>
+                    ))}
+                </ul>
+
+            </div>
+        </div>
     </>
   )
 }
